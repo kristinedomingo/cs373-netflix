28088f602f4f43db1736e29967a450ac43c0c46e
micahramirez <xaviermicahramirez@utexas.edu>
2016-02-08 14:54:01 -0600

    Added base methods for Netflix.py

---
+++ Netflix.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+# ---------------------------
+# projects/collatz/Collatz.py
+# Copyright (C) 2015
+# Glenn P. Downing
+# Xavier Micah Ramirez
+# ---------------------------
+
+# ------------
+# netflix_read
+# ------------
+
+def netflix_read (s) :
+    """
+    read two ints
+    s a string
+    return a list of two ints, representing the beginning and end of a range, [i, j]
+    """
+    a = s.split()
+    return [int(a[0]), int(a[1])]
+
+# ------------
+# netflix_eval
+# ------------
+
+def netflix_eval (i, j) :
+    """
+    i the beginning of the range, inclusive
+    j the end       of the range, inclusive
+    return the max cycle length of the range [i, j]
+    """
+
+    return max_so_far
+
+# -------------
+# netflix_print
+# -------------
+
+def netflix_print (w, i, j, v) :
+    """
+    print three ints
+    w a writer
+    i the beginning of the range, inclusive
+    j the end       of the range, inclusive
+    v the max cycle length
+    """
+    w.write(str(i) + " " + str(j) + " " + str(v) + "\n")
+
+# -------------
+# netflix_solve
+# -------------
+
+def netflix_solve (r, w) :
+    """
+    r a reader
+    w a writer
+    """
+    for s in r :
+        if s != "":
+            i, j = netflix_read(s)
+            v    = netflix_eval(i, j)
+            netflix_print(w, i, j, v)
