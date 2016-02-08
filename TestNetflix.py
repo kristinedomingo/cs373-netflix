333fac7277d9dbe0cd06e788315598a359087b52
micahramirez <xaviermicahramirez@utexas.edu>
2016-02-08 14:44:49 -0600

    Adding code to testnetflix fixes #1

---
+++ TestNetflix.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+# -------------------------------
+# projects/collatz/TestCollatz.py
+# Copyright (C) 2015
+# Glenn P. Downing
+# Xavier Micah Ramirez
+# -------------------------------
+
+# https://docs.python.org/3.4/reference/simple_stmts.html#grammar-token-assert_stmt
+
+# -------
+# imports
+# -------
+
+from io       import StringIO
+from unittest import main, TestCase
+
+from Netflix import netflix_read, netflix_eval, netflix_print, netflix_solve
+
+
+
+# -----------
+# TestNetflix
+# -----------
+
+class TestNetflix (TestCase) :
+
+
+
+# ----
+# main
+# ----
+
+if __name__ == "__main__" :
+    main()
