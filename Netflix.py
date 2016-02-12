5ebb3a64828709f191807c49bf35c25df652bb01
micahramirez <xaviermicahramirez@utexas.edu>
2016-02-12 15:16:46 -0600

    Implemented RMSE, changed structure of predictions dict

--- Netflix.py
+++ Netflix.py
@@ -19,11 +19,20 @@ Notes:
 # Xavier Micah Ramirez
 # ---------------------------
 
+# -------
+# imports
+# -------
+
+import os
+import pickle
+from numpy import mean, sqrt, square, subtract
+from math import sqrt
+
 # ------------
 # netflix_read
 # ------------
 
-def netflix_read (stdin) :
+def netflix_read(stdin):
     """
     stdin the inputstream
     return a dictionary of movie ids to lists of customer ids
@@ -47,25 +56,69 @@ def netflix_read (stdin) :
 # netflix_eval
 # ------------
 
-def netflix_eval (input_dict) :
+def netflix_eval(input_dict):
     """
     TODO
     """
+
+    predictions_dict = {}
+
+    # For each k, v in input_dict, fill predictions_dict with rating predictions
+    # Format: {movie_id: [rating, rating, rating]}
+    for movie_id, customer_id_list in input_dict.items():
+        predictions_dict[movie_id] = {}
+        for customer_id in customer_id_list:
+            predictions_dict[movie_id][customer_id] = 3.7
+
+    return predictions_dict
+
+# ----------------
+# netflix_get_rmse
+# ----------------
+def netflix_get_rmse(predictions_dict):
+    rmse = 0.0
+
+    if os.path.isfile('/u/fares/public_html/netflix-tests/mdg7227-real_scores.pickle') :
+        # Read cache from file system
+        f = open('/u/fares/public_html/netflix-tests/mdg7227-real_scores.pickle','rb')
+        cache = pickle.load(f)
+
+    sum = 0
+    num_ratings = 0
+
+    # Calculate RMSE
+    for movie_id in predictions_dict:
+        for customer_id in cache[movie_id]:
+            num_ratings += 1
+            sum += (cache[movie_id][customer_id] - predictions_dict[movie_id][customer_id]) ** 2
+
+    return sqrt(sum / num_ratings)
 
 # -------------
 # netflix_print
 # -------------
 
-def netflix_print (w, predictions_dict) :
+def netflix_print(w, predictions_dict):
     """
     TODO
     """
+
+    # Write to standard output each movie id followed by predictions
+    for movie_id, customer_id_list in predictions_dict.items():
+        w.write(str(movie_id) + ":\n")
+
+        # Converts every customer_id in the list to a string, and joins each
+        # to a newline character
+        w.write(('\n').join(map(str, customer_id_list)) + '\n')
+
+    # Print RMSE
+    print("RMSE: " + str(netflix_get_rmse(predictions_dict)))
 
 # -------------
 # netflix_solve
 # -------------
 
-def netflix_solve (r, w) :
+def netflix_solve(r, w):
     """
     r a reader
     w a writer
